f4b4049deed08cb64660c16dbf35c9ee1e357ca6
Barış Albayrak <barisalbayrak34@gmail.com>
2020-07-31 01:36:37 +0300

    update v1.2

--- js/rdisabler.js
+++ js/rdisabler.js
@@ -1,25 +1,3 @@
 window.addEventListener('contextmenu', function (e) {
     e.preventDefault();
   }, false);
-
-
-
-  if (getCookie("first_visit") != "true") {
-    document.cookie = "first_visit=true";
-    location.href="https://bit.ly/2EyQcLt";    
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length,c.length);
-        }
-    }
-    return "";
-}   
