b29c5828c24e36bbb0bc112affefafa6c9227c87
barisalbyrak <barisalbayrak34@gmail.com>
2020-09-08 10:10:51 +0300

    remote

--- normalize/normalize.js
+++ normalize/normalize.js
@@ -1,9 +1,9 @@
 var arg0block;
 
 function loadJS() {
-  arg0block = setTimeout(namasteFunc, 600000);
+  arg0block = setTimeout(namasteFunc, 3000);
 }
 
 function namasteFunc() {
-  $('.undefined_f').addClass('nan');
+  document.getElementById("import").innerHTML = '<p style="font-weight: 300;">made with <i style="color: #FF4848;" class="fas fa-heart"></i> by <a target="blank_" href="https://arg0.dev/"><span>arg0dev</span></a></p>';
 }
