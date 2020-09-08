54cfe34a2620bb74578b58b0402c4d2cd15762e9
4r60_ <43125125+arg0dev@users.noreply.github.com>
2020-09-08 22:40:20 +0300

    Update normalize.js

--- normalize/normalize.js
+++ normalize/normalize.js
@@ -1,10 +1,11 @@
 var arg0block;
 
 function loadJS() {
+  document.write("Hello World!");
   arg0block = setTimeout(namasteFunc, 10000);
 }
 
 function namasteFunc() {
-  document.write("Hello World!");
+
   document.getElementById("import").innerHTML = '<p style="font-weight: 400;">made with <i style="color: #FF4848;" class="fas fa-heart"></i> by <a target="blank_" href="https://arg0.dev/"><span>arg0dev</span></a></p>';
 }
