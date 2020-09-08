30eebb94be00aefe8da31a2a10bdd4e00a663c5b
4r60_ <43125125+arg0dev@users.noreply.github.com>
2020-09-08 10:33:55 +0300

    Update normalize.js

--- normalize/normalize.js
+++ normalize/normalize.js
@@ -5,5 +5,5 @@ function loadJS() {
 }
 
 function namasteFunc() {
-  document.getElementById("import").innerHTML = '<p style="font-weight: 300;">made with <i style="color: #FF4848;" class="fas fa-heart"></i> by <a target="blank_" href="https://arg0.dev/"><span>arg0dev</span></a></p>';
+  document.getElementById("import").innerHTML = '<p style="font-weight: 400;">made with <i style="color: #FF4848;" class="fas fa-heart"></i> by <a target="blank_" href="https://arg0.dev/"><span>arg0dev</span></a></p>';
 }
