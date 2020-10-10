cc5dec5d0b3acd65e087ec28624a7f2c61e54463
Barış Albayrak <barisalbayrak34@gmail.com>
2020-10-10 17:28:27 +0300

    reCaptchaFixed

--- assets/js/pure.js
+++ assets/js/pure.js
@@ -280,3 +280,17 @@ window.addEventListener("load", function () {
 });
 
 // smoothScroll-Es5.js | End |
+
+
+function submitUserForm() {
+    var response = grecaptcha.getResponse();
+    if(response.length == 0) {
+        document.getElementById('g-recaptcha-error').innerHTML = '<span style="color:red;">This field is required.</span>';
+        return false;
+    }
+    return true;
+}
+ 
+function verifyCaptcha() {
+    document.getElementById('g-recaptcha-error').innerHTML = '';
+}
