da31a2f733b32b0b14f077b39ee6f9db1e496308
Barış Albayrak <barisalbayrak34@gmail.com>
2020-10-10 17:40:35 +0300

    reCaptchaRemoved

--- assets/js/pure.js
+++ assets/js/pure.js
@@ -281,16 +281,3 @@ window.addEventListener("load", function () {
 
 // smoothScroll-Es5.js | End |
 
-
-function submitUserForm() {
-    var response = grecaptcha.getResponse();
-    if(response.length == 0) {
-        document.getElementById('g-recaptcha-error').innerHTML = '<span style="color:red;">This field is required.</span>';
-        return false;
-    }
-    return true;
-}
- 
-function verifyCaptcha() {
-    document.getElementById('g-recaptcha-error').innerHTML = '';
-}
